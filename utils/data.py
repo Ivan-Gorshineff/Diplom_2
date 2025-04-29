2680ddb47ff9b03b2993e27e9912f126f654e602
Ivan <ivangorshinef@gmail>
2025-04-29 19:39:49 +0300

    added data

---
+++ utils/data.py
@@ -0,0 +1,9 @@
+
+USER_EXISTS_MSG = 'User already exists'
+MISSING_FIELD_IN_USER_FORM_MSG = 'Email, password and name are required fields'
+USER_NOT_FOUND_MSG = 'email or password are incorrect'
+USER_NOT_UPDATED_LOGIN_MSG = 'You should be authorised'
+USER_NOT_UPDATED_EMAIL_MSG = 'User with such email already exists'
+CREATE_ORDER_WITHOUT_INGREDIENTS_MSG = 'Ingredient ids must be provided'
+CREATE_ORDER_WITH_INCORRECT_INGREDIENTS_MSG = 'Internal Server Error'
+ORDER_DONT_GET_WITHOUT_AUTH_MSG = 'You should be authorised''
